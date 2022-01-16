0e585326274b433dbc58036cd1d02729863bb5b0
t1enne <nasir.taov@gmail.com>
2022-01-16 17:00:08 +0100

    major update

---
+++ README.md
@@ -0,0 +1,7 @@
+# My wife's portfolio
+
+Things I want to do:
+- [] fix proj to proj transition
+- [] change the home layout
+- [] change works layout
+- [] language button
---
+++ src/js/Home.js
@@ -0,0 +1,29 @@
+// import 'src/assets/img/*'
+import m from "mithril";
+import Footer from "./Footer";
+import Works from './Works/Works';
+import { loadEvent } from '../../app'
+import { cl } from "./utils";
+import { columnsAnim } from "./Title";
+
+const Home = {
+  oncreate() {
+    if (!window.scroller) {
+      document.body.dispatchEvent(loadEvent({ stop: true, route: 'home' }))
+      cl('.c-scrollbar', 'add', 'hidden')
+
+    } else {
+      document.body.dispatchEvent(loadEvent({ scroll: true, stop: true, route: 'home' }))
+      columnsAnim().play()
+    }
+
+  },
+  view() {
+    return [
+      m(Works),
+      m(Footer)
+    ]
+  }
+}
+
+export default Home
